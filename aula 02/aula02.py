c93cf871d2ac0196cfedc21a1bf40bb38d7914d7
susyfla <susyane.bitencourt@gmail.com>
2024-12-05 11:26:10 -0300

    adicionando arquivo aula 02
    aprendi sobre variável e função

---
+++ aula 02/aula02.py
@@ -0,0 +1,11 @@
+print('hello mundo')
+nome_usuario = 'Susyane' # tipo -> str = string
+idade_usuario = 20 # tipo -> int = integer 
+altura_usuario = 1.65 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = boolean
+print (' seja bem- vindo', nome_usuario)
+print (' sua idade é ', idade_usuario, 'anos')
+print (' você tem',altura_usuario, 'metros.')
+print (' você é maior de idade?', maior_idade)
+
+
